34f626727a73774878cfc2c8de86851fe61cb84c
bitterulf <bitterulf@users.noreply.github.com>
2018-02-09 16:08:42 +0100

    Adding mousetrap

--- public/components/ContextMenuComponent.js
+++ public/components/ContextMenuComponent.js
@@ -2,15 +2,28 @@ const ContextMenuComponent = {
     data: {
         hoverIndex: -1
     },
+    oninit: function(vnode) {
+        vnode.attrs.entries = vnode.attrs.entries || [];
+        vnode.attrs.onclick = vnode.attrs.onclick || function() {};
+
+        vnode.attrs.entries.forEach(function(entry, index) {
+            if (entry.hotkey) {
+                Mousetrap.bind(['shift+' + entry.hotkey], function(e) {
+                    vnode.attrs.onclick(entry, index)
+                    return false;
+                });
+            }
+        });
+
+    },
     view: function(vnode) {
-        const entries = vnode.attrs.entries || [];
-        const onclick = vnode.attrs.onclick || function() {};
+
         const data = vnode.state.data;
 
         return m('div', {
             style: 'font-family: "Lucida Console", Monaco, monospace; border: 1px solid black; width: 200px;',
             onmouseout: function() { data.hoverIndex = -1 }
-        }, entries.map(function(entry, index) {
+        }, vnode.attrs.entries.map(function(entry, index) {
 
             const isHovered = data.hoverIndex == index;
 
@@ -20,7 +33,7 @@ const ContextMenuComponent = {
                     data.hoverIndex = index;
                 },
                 onclick: function() {
-                    onclick(entry, index);
+                    vnode.attrs.onclick(entry, index);
                 }
             }, [
                 m('span', { style: 'display: inline-block; background-color: red; width: 20px; text-align: center; border: 1px solid black; border-radius: 8px; margin: 4px;' }, entry.icon),
